cb0df44f28f72aa37eff9284af45ec04a4081970
Anton Balykov <49350081+CaptainShepard01@users.noreply.github.com>
2024-02-11 22:20:29 +0100

    Update inference.py

--- inference.py
+++ inference.py
@@ -58,26 +58,51 @@ def get_binary_predictions(binary_model, img_paths, batch_size):
 
     img_ids = [Path(x).stem for x in img_paths]
 
-    df = pd.DataFrame({"patientId": img_ids, "pneumonia": probs})
+    df = pd.DataFrame({"patientId": img_ids, "pneumonia": probs, "path": img_paths})
 
     return df
 
 
 # TODO: Implement get_detector_predictions
-def get_detector_predictions(detector_model, img_paths, batch_size, binary_predictions):
+def get_detector_predictions(detector_model, batch_size, binary_predictions):
     """
     Get detector predictions for a list of images
 
     Args:
     detector_model: YOLO model
-    img_paths: list of image paths
     batch_size: batch size for inference
     binary_predictions: dataframe with binary predictions
 
     Returns:
     df: dataframe with image ids and detector predictions
     """
-    pass
+
+    detections = []
+    old_size = 1024
+
+    for i, row in binary_predictions.iterrows():
+        img = row["path"]
+        prediction_string = ""
+
+        if row["pneumonia"] < 1:
+            detections.append({'patientId': row['patientId'], 'PredictionString': prediction_string.strip()})
+            continue
+
+        results = detector_model(img)
+        boxes = results[0].boxes
+        if len(boxes.xywhn) > 1:
+            for xywhn, confidence in zip(boxes.xywhn, boxes.conf):
+                x, y, w, h = xywhn
+                width = int(w * old_size) + 1
+                height = int(h * old_size) + 1
+                x_left = int(x * old_size - width / 2)
+                y_top = int(y * old_size - height / 2)
+
+                prediction_string += f"{confidence:.2f} {x_left} {y_top} {width} {height} "
+
+        detections.append({'patientId': row['patientId'], 'PredictionString': prediction_string.strip()})
+
+    return pd.DataFrame.from_dict(detections)
 
 
 def main(args):
@@ -103,12 +128,10 @@ def main(args):
     detector_model = YOLO(detector_weights)
 
     # get detector predictions
-    df = get_detector_predictions(detector_model, img_paths, batch_size, df)
-
-    df = df.drop(columns=["pneumonia"])
+    df = get_detector_predictions(detector_model, batch_size, df)
 
     # save results
-    df.to_csv("results.csv", index=False)
+    df.to_csv("submission.csv", index=False)
 
 
 if __name__ == "__main__":
