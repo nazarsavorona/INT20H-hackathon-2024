51e96d9c459cf4cc51b5166d198615be845c753b
Nazarii Savorona <nazar.savorona@gmail.com>
2024-02-11 23:04:26 +0200

    feat: add binary predictions part

---
+++ inference.py
@@ -0,0 +1,116 @@
+import argparse
+import torch
+from pathlib import Path
+from ultralytics import YOLO
+import pandas as pd
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--binary-weights",
+        type=str,
+        default="./weights/binary.pt",
+        help="binary weights file",
+    )
+    parser.add_argument(
+        "--detector-weights",
+        type=str,
+        default="./weights/detector.pt",
+        help="detector weights file",
+    )
+    parser.add_argument(
+        "--source",
+        type=str,
+        default="./datasets/pneumonia_inference/images",
+        help="source directory for images",
+    )
+
+    parser.add_argument(
+        "--batch-size",
+        type=int,
+        default=128,
+        help="batch size for inference",
+    )
+    return parser.parse_args()
+
+
+def get_binary_predictions(binary_model, img_paths, batch_size):
+    """
+    Get binary predictions for a list of images
+
+    Args:
+    binary_model: YOLO model
+    img_paths: list of image paths
+    batch_size: batch size for inference
+
+    Returns:
+    df: dataframe with image ids and binary predictions
+    """
+    results = []
+    for i in range(0, len(img_paths), batch_size):
+        batch = img_paths[i : i + batch_size]
+        results.append(binary_model(batch))
+
+    results = [item for sublist in results for item in sublist]
+
+    probs = [result.probs.top1 for result in results]
+
+    img_ids = [Path(x).stem for x in img_paths]
+
+    df = pd.DataFrame({"patientId": img_ids, "pneumonia": probs})
+
+    return df
+
+
+# TODO: Implement get_detector_predictions
+def get_detector_predictions(detector_model, img_paths, batch_size, binary_predictions):
+    """
+    Get detector predictions for a list of images
+
+    Args:
+    detector_model: YOLO model
+    img_paths: list of image paths
+    batch_size: batch size for inference
+    binary_predictions: dataframe with binary predictions
+
+    Returns:
+    df: dataframe with image ids and detector predictions
+    """
+    pass
+
+
+def main(args):
+    batch_size = args.batch_size
+
+    binary_weights = args.binary_weights
+    detector_weights = args.detector_weights
+
+    # Initialize
+    binary_model = YOLO(binary_weights)
+
+    # Get source images
+    source = args.source
+    files = [x for x in Path(source).glob("*")]
+
+    # create dataframe with image paths
+    img_paths = []
+    for file in files:
+        img_paths.append(str(file))
+
+    df = get_binary_predictions(binary_model, img_paths, batch_size)
+
+    detector_model = YOLO(detector_weights)
+
+    # get detector predictions
+    df = get_detector_predictions(detector_model, img_paths, batch_size, df)
+
+    df = df.drop(columns=["pneumonia"])
+
+    # save results
+    df.to_csv("results.csv", index=False)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
